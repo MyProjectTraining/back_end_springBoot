d70854f56036be1a1d3866f202815df3702ab72e
Ny-Antsaniaina <hei.nyantsaniaina@gmail.com>
2026-05-11 17:25:25 +0300

    chore: add class create image product

---
+++ src/main/java/com/example/back_end_java/entity/product/ImageResponse.java
@@ -0,0 +1,14 @@
+package com.example.back_end_java.entity.product;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ImageResponse {
+    private Long id ;
+    private String filename;
+    private String url;
+}
